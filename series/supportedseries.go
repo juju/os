3611fd7132bd676d16187554d2d136e1e3767707
Simon Richardson <stickupkid@gmail.com>
2020-10-29 15:17:11 +0000

    Add hirsute series
    Add hirsute series to the dist info data, although not released, it is
    known by it's code name. Adding it as not supported will suffice for the
    moment.

--- series/supportedseries.go
+++ series/supportedseries.go
@@ -80,6 +80,7 @@ var seriesVersions = map[string]string{
 	"eoan":             "19.10",
 	"focal":            "20.04",
 	"groovy":           "20.10",
+	"hirsute":          "21.04",
 	"win2008r2":        "win2008r2",
 	"win2012hvr2":      "win2012hvr2",
 	"win2012hv":        "win2012hv",
@@ -203,6 +204,10 @@ var ubuntuSeries = map[string]seriesVersion{
 	"groovy": {
 		Version:   "20.10",
 		Supported: true,
+	},
+	"hirsute": {
+		Version:   "21.04",
+		Supported: false,
 	},
 }
 
