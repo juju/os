6f1683b478b94a8ee9116aabb14a3065e4262a3d
Ben Hoyt <ben.hoyt@canonical.com>
2020-11-16 09:15:36 +1300

    Add Big Sur to macOS series list

--- series/series.go
+++ series/series.go
@@ -93,6 +93,7 @@ func macOSXSeriesFromKernelVersion(getKernelVersion func() (string, error)) (str
 // macOSXSeries maps from the Darwin Kernel Major Version to the Mac OSX
 // series.
 var macOSXSeries = map[int]string{
+	20: "bigsur",
 	19: "catalina",
 	18: "mojave",
 	17: "highsierra",
